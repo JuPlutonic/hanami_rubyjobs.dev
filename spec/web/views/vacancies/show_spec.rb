40e903101c64d0c017c182ea58809dcb146fa2fc
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2019-06-26 03:54:17 -1100

    Likes implemented.

--- spec/web/views/vacancies/show_spec.rb
+++ spec/web/views/vacancies/show_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Web::Views::Vacancies::Show, type: :view do
-  let(:exposures) { format: :html, vacancy: vacancy_aggregate, flash: {} } }
+  let(:exposures) { { format: :html, vacancy: vacancy_aggregate, flash: {} } }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/vacancies/show.html.slim') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
@@ -18,5 +18,16 @@ RSpec.describe Web::Views::Vacancies::Show, type: :view do
 
   it 'exposes #format' do
     expect(view.format).to eq exposures.fetch(:format)
+  end
+
+  it 'social buttons showing' do
+    expect(rendered).to match('<div class="likely likely-small">')
+  end
+
+  it 'when head does contain style likely link' do
+    expect(rendered).to match('<link href="/assets/likely.css" type="text/css" rel="stylesheet">')
+  end
+  it 'when app does contain js likely link' do
+    expect(rendered).to match('<script src="/assets/likely.js" type="text/javascript"></script>')
   end
 end
