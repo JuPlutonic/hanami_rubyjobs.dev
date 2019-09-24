4e9b144eb010538fab83e13332c4bd2ca3dcec26
0x639d1dec74 <guhtymailru@mailinator.com>
2019-09-24 20:27:23 +0300

    Users can subscribe to new vacancies (feature spec added).

---
+++ spec/web/features/users_can_subscribe_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'features_helper'
+
+RSpec.describe 'User can subscribe to weekly emails', type: :feature do
+  let(:url) { '' }
+  let(:repo) { SubscriberRepository.new }
+
+  it 'creates a new subscriber object' do
+    visit(url)
+    expect(page.status_code).to eq 200
+  end
+end
