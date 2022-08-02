ac421896c3eb4397667708e6ca3aa44e0430925a
JuPlutonic <JuPlutonic@users.noreply.github.com>
2022-08-02 16:31:35 +0300

    Made puma-related changes

---
+++ config/puma.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+# How many worker processes puma should run
+#
+# Use WEB_CONCURRENCY=0 to disable puma's clustered mode
+workers Integer(ENV["WEB_CONCURRENCY"] || 2)
+# How many threads should exist in each worker's pool
+
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
+# Preload the app to boot the application before forking new workers
+preload_app!
+# Use the default rackup (config.ru) command
+rackup DefaultRackup
+# Specify the port that Puma will listen on
+port ENV.fetch("PORT") { 2300 }
+# Specify the environment Puma will run in
+environment ENV.fetch("HANAMI_ENV") { "development" }
+
+# Shutdown the application before forking
+before_fork do
+  Hanami.shutdown
+end
