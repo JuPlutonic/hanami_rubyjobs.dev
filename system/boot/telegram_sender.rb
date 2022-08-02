c6db47e3a8f3081bb8e1dd39bf6b076bdadf3764
JuPlutonic <JuPlutonic@users.noreply.github.com>
2022-08-02 16:03:21 +0300

    Cr8d notifcation sender with all new vacancies

--- system/boot/telegram_sender.rb
+++ system/boot/telegram_sender.rb
@@ -1,21 +1,17 @@
 # frozen_string_literal: true
 
 require 'telegram/bot'
-# https://t.me/rubyjobs_dev_moderation
 
 class TelegramSender
-  include Dry::Monads::Task::Mixin
 
-  attr_reader :bot
+  attr_reader :bot, :logger
 
   def initialize(token:, logger:)
     @bot = Telegram::Bot::Client.new(token, logger: logger)
   end
 
   def call(chat_id, message)
-    Task do
-      bot.api.send_message(chat_id: chat_id, text: message)
-    end
+    bot.api.send_message(chat_id: chat_id, text: message)
   end
 end
 
